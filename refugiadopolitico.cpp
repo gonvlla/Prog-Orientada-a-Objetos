5c90cad8fcb583be711cebd783c5c444f8d0c3dd
Gonzalo Villalba <144571299+gonvlla@users.noreply.github.com>
2024-07-25 21:43:28 -0300

    Delete refugiadopolitico.cpp

--- refugiadopolitico.cpp
+++
@@ -1,9 +0,0 @@
-#include "refugiadopolitico.h"
-
-RefugiadoPolitico::RefugiadoPolitico(string nac, string fec, string tv, string de, string ec, string ev, string pr,string pv,string vs, string vp,string ant,string req)
-    :Persona(nac,fec,tv,de,ec,ev,pr,pv,vs,vp,ant,req){}
-
-int RefugiadoPolitico::calcularPuntos()
-{
-    return 15;
-}
